5f4fc0df738fb5c52cc122c599b4668651fcfe6e
Mohammed Nafees <nafees.technocool@gmail.com>
2017-09-25 17:48:34 -0400

    Add template file for waw_data

---
+++ waw_data.template.js
@@ -0,0 +1,4 @@
+module.exports.WABrowserId = ""; // Copy-paste your WABrowserId here
+module.exports.WASecretBundle = ""; // Copy-paste your WASecretBundle
+module.exports.WAToken1 = ""; // Copy-paste your WAToken1
+module.exports.WAToken2 = ""; // Copy-paste your WAToken2
